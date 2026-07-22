e6109ed12cf8729fbef8e06aaecbf73bcafeeb14
Mahalath <mahalath001@gmail.com>
2026-07-22 10:04:33 +0100

    adding more configs

--- terraform-configs/argocd.tf
+++ terraform-configs/argocd.tf
@@ -1,15 +1,15 @@
 resource "helm_release" "argocd" {
-    name = "argocd"
-    chart = "argo-cd"
-    repository = "https://argoproj.github.io.argo-helm"
-    namespace = "argocd"
-    create_namespace = "true"
+  name             = "argocd"
+  chart            = "argo-cd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  namespace        = "argocd"
+  create_namespace = "true"
 
-    values = [
-        <<EOF
+  values = [
+    <<EOF
         server:
             service:
                 type: ClusterIP
         EOF
-    ]
+  ]
 }
